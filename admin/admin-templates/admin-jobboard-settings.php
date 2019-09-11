5cd89f3e64dd16b9640a706522e9e2ef3d064fce
Abe Caymo <caymo.abesuni@gmail.com>
2019-09-11 11:09:52 +0800

    fixed settings page

--- admin/admin-templates/admin-jobboard-settings.php
+++ admin/admin-templates/admin-jobboard-settings.php
@@ -1,6 +1,6 @@
 <?php settings_errors(); ?>
 <form method="post" action="options.php" class='revolt-job-board-form'>
     <?php settings_fields('revolt-jobboard-settings'); ?>
-    <?php do_settings_sections('revolt_jobboard'); ?>
+    <?php do_settings_sections('revolt_jobboard_settings_page'); ?>
     <?php submit_button(); ?>
 </form>
