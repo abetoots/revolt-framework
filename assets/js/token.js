e6aed7f2b6e0bdede9fe4bf71da17ab9ce6ca2fd
Abe Caymo <caymo.abesuni@gmail.com>
2019-09-12 14:56:26 +0800

    get token when employer, to be used with react

---
+++ assets/js/token.js
@@ -0,0 +1,35 @@
+(function ($) {
+    $(document).ready(function () {
+        $('#login-form').submit(function (event) {
+            let form = this;
+            $('.Login__submitBtn').attr('disabled', true).css('cursor', 'wait');
+            event.preventDefault();
+            let userName = $('#user_login').val();
+            let pass = $('#user_pass').val();
+            fetch(`http://localhost/flerson/wp-json/simple-jwt-authentication/v1/token?username=${userName}&password=${pass}`, {
+                method: 'POST'
+            })
+                .then(res => {
+                    if (res.ok) {
+                        return res.json();
+                    } else {
+                        throw (res.statusText);
+                    }
+                })
+                .then(data => {
+                    if (data.role === 'employer') {
+                        const expirationDate = new Date(data.token_expires * 1000);
+                        localStorage.setItem('token', data.token);
+                        localStorage.setItem('userId', data.user_id);
+                        localStorage.setItem('expirationDate', expirationDate);
+                        localStorage.setItem('userName', data.username);
+                        localStorage.setItem('userRole', data.role);
+                    }
+                    form.submit();
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        })
+    });
+})(jQuery);
