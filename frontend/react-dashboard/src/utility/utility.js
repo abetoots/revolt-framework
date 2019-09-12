51eac7b1aee7a50df26960ee513fdb68d57836d0
Abe Caymo <caymo.abesuni@gmail.com>
2019-09-12 15:01:46 +0800

    delete unused, updated checkvalidity

--- frontend/react-dashboard/src/utility/utility.js
+++ frontend/react-dashboard/src/utility/utility.js
@@ -1,6 +1,3 @@
-//* The prefix I use for my meta fields in WordPress
-export const prefix = 'revolt_';
-
 export const updateObject = (oldState, newProperties) => {
     return {
         ...oldState,
@@ -8,37 +5,46 @@ export const updateObject = (oldState, newProperties) => {
     }
 }
 
-export const checkValidity = (value, rules) => {
+export const checkValidity = (value, field) => {
     let isValid = true;
     // console.log(isValid);
-    if (rules.required) {
-        isValid = value.trim() !== '' && isValid;
+    if (field.required === 1) {
+        isValid = value !== '' && isValid;
+    }
+    let regex = '';
+    switch (field.type) {
+
+        case ('email'):
+            regex = new RegExp('^(([^<>()\\[\\]\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$');
+            isValid = regex.test(value) === true && isValid;
+            break;
+
+        case ('number'):
+            field.label === 'Phone Number' ?
+                regex = new RegExp('^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$', 'g') :
+                regex = new RegExp('^[0-9]*$', 'gm');
+            isValid = regex.test(value) === true && isValid;
+            break;
+
+        case ('url'):
+            regex = new RegExp('((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[\\-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9\\.\\-]+|(?:www\\.|[\\-;:&=\\+\\$,\\w]+@)[A-Za-z0-9\\.\\-]+)((?:\\/[\\+~%\\/\\.\\w\\-_]*)?\\??(?:[\\-\\+=&;%@\\.\\w_]*)#?(?:[\\.\\!\\/\\\\w]*))?)');
+            isValid = regex.test(value) === true && isValid;
+            break;
+
+        default:
+            return isValid;
+
     }
 
-    if (rules.isEmail) {
-        let regex = new RegExp('^(([^<>()\\[\\]\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$');
-        isValid = regex.test(value) === true && isValid;
+    if (field.min) {
+        isValid = value.length >= field.min && isValid;
     }
 
-    if (rules.isPhoneNum) {
-        let regex = new RegExp('^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$', 'g');
-        isValid = regex.test(value) === true && isValid;
+    if (field.max) {
+        isValid = value.length <= field.max && isValid;
     }
 
-    if (rules.isUrl) {
-        let regex = new RegExp('((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[\\-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9\\.\\-]+|(?:www\\.|[\\-;:&=\\+\\$,\\w]+@)[A-Za-z0-9\\.\\-]+)((?:\\/[\\+~%\\/\\.\\w\\-_]*)?\\??(?:[\\-\\+=&;%@\\.\\w_]*)#?(?:[\\.\\!\\/\\\\w]*))?)');
-        isValid = regex.test(value) === true && isValid;
-    }
-
-    if (rules.minLength) {
-        isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.maxLength) {
-        isValid = value.length <= rules.maxLength && isValid;
-    }
-
-    if (rules.isTwitter) {
+    if (field.twitterValidate) {
         let regex = new RegExp('^@?(\\w){1,15}$');
         isValid = regex.test(value) === true && isValid;
     }
