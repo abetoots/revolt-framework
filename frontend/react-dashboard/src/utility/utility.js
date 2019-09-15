e73038ff04fb1bac75fbaeadc81275f820c1d173
Abe Caymo <caymo.abesuni@gmail.com>
2019-09-15 16:35:34 +0800

    separate validity check for acf fields and normal input

--- frontend/react-dashboard/src/utility/utility.js
+++ frontend/react-dashboard/src/utility/utility.js
@@ -5,7 +5,7 @@ export const updateObject = (oldState, newProperties) => {
     }
 }
 
-export const checkValidity = (value, field) => {
+export const checkFieldValidity = (value, field) => {
     let isValid = true;
     // console.log(isValid);
     if (field.required === 1) {
@@ -45,6 +45,44 @@ export const checkValidity = (value, field) => {
     }
 
     if (field.twitterValidate) {
+        let regex = new RegExp('^@?(\\w){1,15}$');
+        isValid = regex.test(value) === true && isValid;
+    }
+
+    return isValid;
+}
+
+export const checkInputValidity = (value, rules) => {
+    let isValid = true;
+
+    if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.isEmail) {
+        let regex = new RegExp('^(([^<>()\\[\\]\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$');
+        isValid = regex.test(value) === true && isValid;
+    }
+
+    if (rules.isPhoneNum) {
+        let regex = new RegExp('^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$', 'g');
+        isValid = regex.test(value) === true && isValid;
+    }
+
+    if (rules.isUrl) {
+        let regex = new RegExp('((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[\\-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9\\.\\-]+|(?:www\\.|[\\-;:&=\\+\\$,\\w]+@)[A-Za-z0-9\\.\\-]+)((?:\\/[\\+~%\\/\\.\\w\\-_]*)?\\??(?:[\\-\\+=&;%@\\.\\w_]*)#?(?:[\\.\\!\\/\\\\w]*))?)');
+        isValid = regex.test(value) === true && isValid;
+    }
+
+    if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isTwitter) {
         let regex = new RegExp('^@?(\\w){1,15}$');
         isValid = regex.test(value) === true && isValid;
     }
