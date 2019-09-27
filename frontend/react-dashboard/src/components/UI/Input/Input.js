ee2b54670c4f091631e278df97bf0a1c3d88cae5
Abe Caymo <caymo.abesuni@gmail.com>
2019-09-27 15:07:36 +0800

    move acf input logic to separate component

--- frontend/react-dashboard/src/components/UI/Input/Input.js
+++ frontend/react-dashboard/src/components/UI/Input/Input.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './Input.scss';
 
-import SwitchToggle from '../SwitchToggle/SwitchToggle';
-
 const Input = (props) => {
     let inputElement = null;
     //add invalid css class 'invalid' when invalid for better ux
@@ -10,107 +8,6 @@ const Input = (props) => {
     if (!props.valid && props.touched && props.shouldValidate) {
         inputClasses.push('-invalid');
     }
-
-    //switch case for acf inputs
-    if (props.inputType) {
-        switch (props.inputType) {
-            case ('true_false'):
-                inputElement =
-                    <SwitchToggle
-                        classes={inputClasses.join(' ')}
-                        value={props.value}
-                        onChange={props.changed}
-                        checked={props.value}
-                    />
-                break;
-
-            case ('taxonomy'):
-                // debugger;
-                switch (props.fieldType) {
-                    case ('radio'):
-                        inputElement =
-                            <ul className="Input__ul">
-                                {props.taxonomyOptions.map(term => (
-                                    <li key={term.slug}>
-                                        <input
-                                            id={term.slug}
-                                            name={term.taxonomy}
-                                            type="radio"
-                                            value={term.id}
-                                            checked={props.value === term.id}
-                                            onChange={props.changed}
-                                        />
-                                        <label htmlFor={term.slug}>{term.name}</label>
-                                    </li>
-                                ))}
-                            </ul>
-
-                        break;
-                    case ('checkbox'):
-                        inputElement =
-                            <ul className="Input__ul">
-                                {props.taxonomyOptions.map(term => (
-                                    <li key={term.slug}>
-                                        <input
-                                            id={term.slug}
-                                            name={term.taxonomy}
-                                            type="checkbox"
-                                            value={term.id}
-                                            checked={props.value.includes(term.id)}
-                                            onChange={props.changed}
-                                        />
-                                        <label htmlFor={term.slug}>{term.name}</label>
-                                    </li>
-                                ))}
-                            </ul>
-                        break
-                    case ('select'):
-                        inputElement =
-                            <select
-                                value={props.value}
-                                onChange={props.changed}>
-                                {props.options.map(option => (
-                                    <option
-                                        key={option.value}
-                                        value={option.value}>
-                                        {option.displayValue}
-                                    </option>
-                                ))}
-                            </select>
-                        break;
-                    default:
-                        return;
-                }
-
-                break;
-
-            case ('radio'):
-                inputElement =
-                    <ul className="Input__ul">
-                        {props.defaultChoices.map(([value, label]) => (
-                            <li key={value}>
-                                <input
-                                    id={value}
-                                    name={props.name}
-                                    type="radio"
-                                    value={label}
-                                    checked={props.value === label}
-                                    onChange={props.changed}
-                                />
-                                <label htmlFor={value}>{label}</label>
-                            </li>
-                        ))}
-                    </ul>
-                break;
-            default:
-                inputElement = <input
-                    type={props.inputType}
-                    className={inputClasses.join(' ')}
-                    value={props.value}
-                    onChange={props.changed}
-                    placeholder="Default placeholder" />;
-        }
-    } //end switch case for acf inputs
 
     //switch case for normal inputs
     if (props.elementType) {
