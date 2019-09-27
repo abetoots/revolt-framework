1dadd3c525e86dfe75cd8e471c948160dbf031df
Abe Caymo <caymo.abesuni@gmail.com>
2019-09-27 15:07:17 +0800

    naming semantics, add new job button

--- frontend/react-dashboard/src/components/Sidebar/Navigation/Navigation.js
+++ frontend/react-dashboard/src/components/Sidebar/Navigation/Navigation.js
@@ -6,19 +6,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Navigation = (props) => {
     let navigationItems =
         <ul className="Navigation__list">
-            <div className="Nav__block">
+            <div className="Navigation__slot">
+                <NavigationItem link="/dashboard/jobs/new" exact>
+                    <button className="Navigation__btn">
+                        <FontAwesomeIcon icon={['fas', 'location-arrow']} className="Navigation__icon" />
+                        <span>Post New Job</span>
+                    </button>
+                </NavigationItem>
+            </div>
+            <div className="Navigation__slot">
                 <NavigationItem link="/dashboard/overview" exact >
                     <FontAwesomeIcon icon={['fas', 'th']} className="Navigation__icon" />
                     <span>Overview</span>
                 </NavigationItem>
             </div>
-            <div className="Navigation__block">
+            <div className="Navigation__slot">
                 <NavigationItem link="/dashboard/hire" exact>
                     <FontAwesomeIcon icon={['fas', 'user-friends']} className="Navigation__icon" />
                     <span>Browse Candidates</span>
                 </NavigationItem>
             </div>
-            <div className="Navigation__block">
+            <div className="Navigation__slot">
                 <NavigationItem link="/dashboard/jobs" exact>
                     <FontAwesomeIcon icon={['fas', 'briefcase']} className="Navigation__icon" />
                     <span>Your Jobs</span>
